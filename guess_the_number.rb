08729f854848994fd0516b9c9d54c3500b7cefae
Cristian Pisco <cristian.pisco@outlook.com>
2022-12-16 00:17:26 -0500

    refactorizando el mini juego

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -1,37 +1,68 @@
-class RandomNumber
-    FROM = 1
-    TO = 100
-    def get_random_number()
-        rand FROM..TO
+class GuessNumber
+    attr_reader :number
+    attr_reader :from
+    attr_reader :to
+
+    def initialize(from = 1, to = 100)
+        @from = from
+        @to = to
     end
-end
-print("Enter your name: ")
-name = gets.chomp
-puts("Welcome #{name}")
-rnd = RandomNumber.new
-number = rnd.get_random_number
-is_guesssed = false
-TRIES = 10
-counter = 0
-while (!is_guesssed && counter < TRIES)
-    print("Guess the number: ")
-    guess_number = gets.chomp.to_i
-    if guess_number > number
-        puts("Oops. Your guess was HIGH")
-        is_guesssed = false
-        counter+=1
-        puts("You have #{counter} tries")
-    elsif guess_number < number
-        puts("Oops. Your guess was LOW")
-        is_guesssed = false
-        counter+=1
-        puts("You have #{counter} tries")
-    else
-        puts("YIKES...")
-        is_guesssed = true
+
+    def set_random_number()
+        @number = rand @from..@to
+    end
+
+    def is_guessed(number_entered)
+        if number_entered > number
+            puts("Oops. Your guess was HIGH")
+            return false
+        elsif number_entered < number
+            puts("Oops. Your guess was LOW")
+            return false
+        else
+            return true
+        end
     end
 end
 
-if (counter >= TRIES)
-    puts("Sorry... Try again")
+class App
+    attr_reader :tries
+    attr_reader :client_name
+    attr_reader :counter
+
+    def initialize(tries = 10)
+        @tries = tries
+        print("Enter your name: ")
+        @client_name = gets.chomp
+        puts("Welcome #{@client_name}")
+        @counter = 0
+    end
+
+    def run
+        rnd = GuessNumber.new
+        rnd.set_random_number
+        is_guesssed = false
+
+        while (!is_guesssed && @counter < @tries)
+            print("Guess the number: ")
+            guess_number = gets.chomp.to_i
+            is_guesssed = rnd.is_guessed(guess_number)
+            if (is_guesssed)
+                puts("YIKES...")
+            else
+                @counter+=1
+                puts("You have #{@counter} tries")
+            end
+            if (@counter >= @tries)
+                puts("Sorry... Try again")
+            end
+        end
+    end
+    
+    def reset
+        @counter = 0
+    end
 end
+
+app = App.new
+app.run()
